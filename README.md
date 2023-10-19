2c0454c83307cea6528630407e6ee734c11dfae6
matheusLazaroCC-UFG <matheus.lazaro@discente.ufg.br>
2023-10-18 22:09:05 -0300

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-teste7777
+# Implementação de Transformações Básicas em OpenGL
+
+- Este código em C demonstra a implementação de transformações básicas - translação, rotação e escala - em um ponto usando a biblioteca OpenGL. As transformações são executadas de forma interativa, permitindo ao usuário escolher o tipo de transformação a ser aplicada.
+
+## Funções Implementadas
+
+### Translação
+
+A translação é o processo de mover um objeto de uma posição para outra em um sistema de coordenadas. A função `transladarPonto` implementa a translação de um ponto, permitindo que ele se mova horizontalmente (tx) e verticalmente (ty).
+
+### Rotação
+
+A rotação envolve girar um objeto em torno de um ponto específico. A função `rotacionarEmTornoDoPonto` realiza a rotação de um ponto em torno de um ponto central (cx, cy) e permite que o ponto gire de forma interativa.
+
+### Escala
+
+A escala envolve redimensionar um objeto em relação a um ponto específico. A função `escalarPonto` implementa a escala de um ponto, permitindo que ele seja redimensionado em relação a um ponto central.
+
+## Fórmulas Geométricas
+
+- **Translação**: Novo Ponto (px, py) = Ponto Original (px, py) + Deslocamento (tx, ty)
+
+- **Rotação**: Novo Ponto (xf, yf) = (cx + (px - cx) * cos(theta) - (py - cy) * sin(theta), cy + (px - cx) * sin(theta) + (py - cy) * cos(theta)
+
+- **Escala**: Novo Ponto (fx, fy) = (px * Fator de Escala Horizontal (sx), py * Fator de Escala Vertical (sy)
+
+## Como Executar
+
+Compile o código e execute o programa para escolher entre as opções de translação, rotação ou escala.
+
+```bash
+$ gcc -c translacao2D.c -lGL -lGLU -lglut -lm
+$ gcc -o translacao2D translacao2D.c -lGL -lGLU -lglut -lm
+$ ./translacao2D
+```
--- translacao2D.c
+++ translacao2D.c
@@ -12,16 +12,14 @@
 #define velocidadeTheta 0.005
 
 // Isso cria um atraso entre duas ações
-void atraso(unsigned int msegundos)
-{
+void atraso(unsigned int msegundos) {
     clock_t objetivo = msegundos + clock();
     while (objetivo > clock());
 }
 
 // Esta é uma inicialização básica para a janela glut
-void minhaInicializacao(void)
-{
-    glClearColor(1.0, 1.0, 1.0, 0.0);
+void minhaInicializacao(void){
+    glClearColor(1.0, 1.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0.0, maxLargura, 0.0, maxAltura);
@@ -30,20 +28,17 @@ void minhaInicializacao(void)
 }
 
 // Esta função apenas desenha um ponto
-void desenharPonto(int x, int y)
-{
-    glPointSize(7.0);
+void desenharPonto(int x, int y) {
+    glPointSize(10.0);
     glColor3f(0.0f, 0.0f, 1.0f);
     glBegin(GL_POINTS);
     glVertex2i(x, y);
     glEnd();
 }
 
-void rotacionarEmTornoDoPonto(int px, int py, int cx, int cy)
-{
+void rotacionarEmTornoDoPonto(int px, int py, int cx, int cy) {
     float theta = 0.0;
-    while (1)
-    {
+    while (1) {
         glClear(GL_COLOR_BUFFER_BIT);
         int xf, yf;
 
@@ -70,17 +65,20 @@ void rotacionarEmTornoDoPonto(int px, int py, int cx, int cy)
         glFlush();
 
         // Criando um atraso para que o ponto possa ser notado
-        atraso(10);
+        atraso(100);
     }
 }
 
 // Esta função irá transladar o ponto
-void transladarPonto(int px, int py, int tx, int ty)
-{
+void transladarPonto(int px, int py, int tx, int ty) {
+    int tempx = px;
+    int tempy = py;
     int fx = px, fy = py;
-    while (1)
-    {
+    while (1)  {
         glClear(GL_COLOR_BUFFER_BIT);
+
+// Desenhando o ponto
+        desenharPonto(px, py);
 
         // Atualizar
         px = px + tx;
@@ -88,29 +86,23 @@ void transladarPonto(int px, int py, int tx, int ty)
 
         // Verificar overflow para manter o ponto na tela
         if (px > maxLargura || px < 0 ||
-            py > maxAltura || py < 0)
-        {
+            py > maxAltura || py < 0) {
             px = fx;
             py = fy;
         }
 
-        // Desenhando o ponto
-        desenharPonto(px, py);
-
         glFlush();
 
         // Criando um atraso para que o ponto possa ser notado
-        atraso(10);
+        atraso(1000000);
     }
 }
 
 // Esta função desenha 
-void escalarPonto(int px, int py,
-                  int sx, int sy)
-{
+void escalarPonto(int px, int py, int sx, int sy) {
     int fx, fy;
-    while (1)
-    {
+   
+    while (1) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Atualizar 
@@ -142,17 +134,17 @@ void escalarPonto(int px, int py,
 }
 
 // Função de exibição real 
-void minhaExibicao(void)
-{
+void minhaExibicao(void) {
 	int opt;
 	printf("\nDigite\n\t<1> para translação"
 		   "\n\t<2> para rotação"
 		   "\n\t<3> para escala\n\t:");
 	scanf("%d", &opt);
 	printf("\nVá para a janela...");
+
 	switch (opt){
 	case 1:
-		transladarPonto(100, 200, 1, 5);
+		transladarPonto(0, 0, 100, 100);
 		break;
 	case 2:
 		rotacionarEmTornoDoPonto(200, 200, maxLargura / 2,
@@ -167,8 +159,7 @@ void minhaExibicao(void)
 }
 
 // Código principal 
-void main(int argc, char** argv)
-{
+void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(maxLargura, maxAltura);
@@ -178,80 +169,3 @@ void main(int argc, char** argv)
 	minhaInicializacao();
 	glutMainLoop();
 }
-
-
-/*#include <GL/glut.h>
-#include <string.h>
-
-// Função que desenha um quadrado vermelho
-void desenhaQuadrado() {
-    glColor3f(1.0, 0.0, 0.0); // Define a cor vermelha
-    glBegin(GL_QUADS); // Inicia o modo de desenho de quadriláteros
-        glVertex2f(-0.5, -0.5); // Define o primeiro vértice
-        glVertex2f(-0.5, 0.5); // Define o segundo vértice
-        glVertex2f(0.5, 0.5); // Define o terceiro vértice
-        glVertex2f(0.5, -0.5); // Define o quarto vértice
-    glEnd(); // Termina o modo de desenho
-}
-
-// Função que aplica uma transformação de translação 2D
-void transladar(float tx, float ty) {
-    // Calcula a matriz de translação
-    float m[3][3] = {
-        { 1, 0, tx },
-        { 0, 1, ty },
-        { 0, 0, 1 }
-    };
-
-    // Converte o ponteiro para a matriz m para um ponteiro para uma matriz de 4x4
-    float m4x4[4][4] = {
-        { m[0][0], m[0][1], 0, m[0][2] },
-        { m[1][0], m[1][1], 0, m[1][2] },
-        { 0      , 0      , 1, 0       },
-        { m[2][0], m[2][1], 0, m[2][2] }
-    };
-
-    // Multiplica a matriz de transformação atual pela matriz de translação
-    glMultMatrixf(&m4x4[0][0]);
-}
-
-// Função que desenha a cena
-void display() {
-    // Limpa a tela com a cor de fundo
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // Seleciona a matriz de modelagem
-    glMatrixMode(GL_MODELVIEW);
-
-    // Carrega a matriz identidade
-    glLoadIdentity();
-
-    // Aplica uma translação de (0.5, 0.5) no eixo x e y
-    transladar(10, 10);
-
-    // Desenha o quadrado vermelho
-    desenhaQuadrado();
-
-    // Força o desenho na tela
-    glFlush();
-}
-
-// Função principal
-int main(int argc, char** argv) {
-    glutInit(&argc, argv); // Inicializa o GLUT
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Define o modo de exibição
-    glutInitWindowSize(500, 500); // Define o tamanho da janela
-    glutInitWindowPosition(100, 100); // Define a posição inicial da janela
-    glutCreateWindow("Translacao 2D"); // Cria a janela com o título dado
-    glClearColor(1.0, 1.0, 1.0, 1.0); // Define a cor de fundo como branca
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0); // Define o sistema de coordenadas ortogonal 2D
-
-    // Declara a função de desenho da cena
-    glutDisplayFunc(display);
-
-    // Entra no loop principal do GLUT
-    glutMainLoop();
-    
-    return 0;
-}
-*/
